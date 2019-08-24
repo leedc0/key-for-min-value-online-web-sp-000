31ef2ae6cacad97e28a5f452455a075efc0807ab
Daniel Lee <leedc@me.com>
2019-08-24 05:53:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,15 +5,24 @@ def key_for_min_value(name_hash)
 
   name, num = name_hash.first
 
-  if name_hash == {}
-    nil
-  else
-    name_hash.each do |key, value|
-      if value < num
-        num = value
-        name = key
-      end
+#   if name_hash == {}
+#     nil
+#   else
+#     name_hash.each do |key, value|
+#       if value < num
+#         num = value
+#         name = key
+#       end
+#     end
+#   end
+#   name
+# end
+
+  name_hash.each do |key, value|
+    if value < num || name_hash == {}
+      num = value
+      name = key
     end
   end
-  name
+name
 end
